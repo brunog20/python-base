64a75a502046e284a3c01f902b777618c7c27d57
Bruno Alexandre Goncalves <bruno@vm.vm>
2023-01-24 16:33:45 -0300

    Inclusao o hello world multi linguias

--- hello.py
+++ hello.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python3
 
-#Comentários: COM # + ! -  Envia mensagem ao SO  - SHEBANG
+"""Hello Word Multi Linguas.
 
-print ("Hello, World!")
+Dependendo da Lingua configurada no ambiente
+ o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG-pt_BR
+
+Execucao:
+
+   python3 hello.py
+   ou
+   ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ = "Bruno Alexandre"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_GB")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Holla, Mundo!"    
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+print (msg)
